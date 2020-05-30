f93169e911658b5a45471c749470b0de5d6c5468
luchomoreno <lucho.moreno@live.com>
2020-05-29 23:36:33 -0300

    COPIA DE SEGURIDAD

--- src/app/paginas/alta-supervisor/alta-supervisor.page.ts
+++ src/app/paginas/alta-supervisor/alta-supervisor.page.ts
@@ -1,37 +1,112 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ComplementosService } from 'src/app/servicios/complementos.service';
+
+// QR
+
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+
+// PLATFORM
+
+// IMPORTO EL PLATFORM 
+import { Platform } from '@ionic/angular';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-alta-supervisor',
   templateUrl: './alta-supervisor.page.html',
   styleUrls: ['./alta-supervisor.page.scss'],
 })
+
 export class AltaSupervisorPage implements OnInit {
+
+
+  // GLOBALES
+  qrScan:any;
+
 
   // Variables 
   nombre : string;
   apellido : string;
-  dni : string ;
+  @Input() dni  : string ;
   cuil : string; 
   foto : string;
   
-  constructor(public complementos  : ComplementosService) { }
+
+  constructor(private complementos : ComplementosService, 
+    public qrScanner: QRScanner, 
+    public plataform:Platform,
+    ) 
+    {
+      this.plataform.backButton.subscribeWithPriority(0,() => {
+        document.getElementsByTagName("body")[0].style.opacity = "0";
+        this.qrScan.unsubscribe();
+      })
+    }
 
   ngOnInit() {
   }
 
+
+  
   escanearDni()
   {
-    // Hay que validar que el dni ingresado 
-    let dniNuevo = this.complementos.escanearDni();
-    this.dni = dniNuevo;
 
-    alert("COMPLEMENTO:" + this.complementos.escanearDni());
-    alert("DNI IGUALADO:" + this.dni);
-    alert("DNI NUEVO:" + dniNuevo);
+    let texto : string;
+
+    this.qrScanner.prepare()
+
+    .then((status:QRScannerStatus) => {
+
+      if(status.authorized)
+      {
+
+        this.qrScanner.show();
+
+        document.getElementsByTagName("body")[0].style.opacity = "0";
+
+        this.qrScan = this.qrScanner.scan().subscribe((texto) => {
+          
+        document.getElementsByTagName("body")[0].style.opacity = "1";
+        
+        alert('Scanned something: ' + texto);
+
+        this.qrScanner.hide(); // hide camera preview
+        this.qrScan.unsubscribe(); // stop scanning
+        
+
+        //this.complementos.presentToastConMensajeYColor(texto + "Desde el servicio", "danger");
+        //return texto;
+
+
+        if (texto != null)
+        {
+          this.dni = texto;
+          this.qrScanner.destroy();
+          this.dni = this.dni;
+        }
+
+        this.dni = this.dni;
+
+        },(err) =>{
+          //return err;
+          alert(JSON.stringify(err));
+        })
+
+      }
+      else if(status.denied)
+      {
+        //return status.denied;
+      }
+      else{
+        //return "Hubo un error!";
+      }
+
+    })
+
   
 
   }
+
 
 
 }
