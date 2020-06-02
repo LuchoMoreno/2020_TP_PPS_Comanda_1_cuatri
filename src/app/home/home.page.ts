a5e9cb8e571da7897f2218778cdc5b645f5e63f0
Veronica Bustos <veronicabustoss1998@yahoo.com>
2020-06-02 01:28:45 -0300

    Agrego pagina alta-empleado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 // IMPORTO EL ROUTER COMO ULTIMO PASO.
 import { Router } from "@angular/router";
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +12,29 @@ import { Router } from "@angular/router";
 })
 export class HomePage {
 
+  perfilUsuario : string;
 
-  constructor(private router : Router ) {}
+  constructor(private router : Router,
+    private menu: MenuController ) {  }
 
 
-  redireccionPrueba()
-  {
-  this.router.navigate(['/alta-supervisor']);
+
+  ngOnInit() {
+
+    let auxUsuario = JSON.parse(localStorage.getItem("usuario"));
+    console.log(auxUsuario);
   }
+
+
+  redireccionPrueba(perfilUsuario)
+  {
+    if(perfilUsuario == 'supervisor' || perfilUsuario == 'duenio')
+    {
+      this.router.navigate(['/alta-supervisor']);
+    }
+
+  }
+
+
   
 }
--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -36,16 +36,6 @@ export class LoginPage implements OnInit {
     {id:6, correo:"anonimo@anonimo.com", clave:"666666", perfil:"anonimo"}
    ]
 
- /* email : string;
-  password : string;
-
-
-  users: Usuario[] = [{ id: 1, email: "admin@admin.com", password: "111111", perfil: "admin", sexo: "femenino" },
-  { id: 2, email: "invitado@invitado.com", password: "222222", perfil: "invitado", sexo: "femenino" },
-  { id: 3, email: "usuario@usuario.com", password: "333333", perfil: "usuario", sexo: "masculino" },
-  { id: 4, email: "anonimo@anonimo.com", password: "444444", perfil: "anonimo", sexo: "masculino" },
-  { id: 5, email: "tester@tester.com", password: "555555", perfil: "tester", sexo: "femenino" }]
-  */
 
   constructor(
   private authService : AuthService,
@@ -110,7 +100,7 @@ pickerUser(pickedName){
     {
       this.email=user.correo;
       this.password=user.clave;
-      //localStorage.setItem("usuario",JSON.stringify(user));
+      localStorage.setItem("usuario",JSON.stringify(user));
       return;
     }
   })
